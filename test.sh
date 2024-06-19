7338ea8084745960b9b623a5a44cc5ab7a8813cd
Luciano Branco <thelucianobranco@gmail.com>
2024-06-18 21:47:46 -0300

    Delete test.sh

--- test.sh
+++
@@ -1,4 +0,0 @@
- #!/bin/sh
-luna-send -f -n 1 luna://com.webos.notification/createToast '{"message": "<b>PPLGPwn test message</b><br/>Project by: <br/>zauceee and llbranco."}'
-
-echo 'just a test message'
